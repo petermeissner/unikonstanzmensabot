131a6cb1c81cd5486914d6c097ecc8722882c180
petermeissner <retep.meissner@gmail.com>
2015-10-07 08:36:38 +0200

    tweeting

--- R/tweeting.R
+++ R/tweeting.R
@@ -1,11 +1,14 @@
 #' function for generating tweets
-
-dings <- 1
-if(1==2){
-
-  gen_tweets <- function(date=Sys.Date(), lang="de", loc="mensa_giessberg", types="all", regex="^.*", invert=FALSE){}
+#' @param date date for which tweets shall be generated
+#' @param lang language of tweets
+#' @param loc location of mensa
+#' @param dish types
+#' @param regex possible regex for further subsetting
+#' @param invert shall regex be inverted?
+gen_tweets <- function(date=Sys.Date(), lang="de", loc="mensa_giessberg", types="all", regex="^.*", invert=FALSE){
+  # get data
   mpdat <- mp_data()
-
+  # subset data
   iffer <-
     mpdat$date_dish %in% date &
     !grepl("[kK]eine[ ]*Ausgabe", mpdat$dish) &
@@ -15,22 +18,19 @@ if(1==2){
     mpdat$http_status == 200 &
     (mpdat$types %in% types | types == "all") &
     !invert*grepl(regex, apply(mpdat, 1, paste, collapse=" "))
-
   mpdat <- mpdat[iffer,]
-
+  # gen tweets
   tweets <-
     stringr::str_replace_all(
-      paste0(mpdat$types, ": ", mpdat$dish),
+      paste0("[",mpdat$types,"]", " ", mpdat$dish),
       " *\\(.*?\\)",
       ""
     )
   tweets <- data.frame( tweet=ifelse(nchar(tweets) > 140, paste0(substring(tweets, 1, 136), " ..."), tweets ),
                         stringsAsFactors = FALSE)
   tweets$nchar <- nchar(tweets$tweet)
-  tweets <- cbind(tweets, mpdat[, c("types","","")])
-    mpdat
-
-
-
-
+  # combine with other data
+  tweets <- cbind(tweets, mpdat[, c("location","date_dish","language","types")])
+  # return
+  return(tweets)
 }
