97b2603027334bb5c1db2d5177513bd3e71c36dd
petermeissner <retep.meissner@gmail.com>
2015-10-09 22:19:46 +0200

    tools : httr_content_as_list function that is an as.list modification with name prefixing and NULL to "" functionality

--- R/tools.R
+++ R/tools.R
@@ -6,4 +6,16 @@ odd  <- function(x) x[(seq_len(floor(length(x)/2)))*2]
 #' @param x for which to give back odd index
 even <- function(x) x[(seq_len(ceiling(length(x)/2)))*2-1]
 
+#' function for making lists with prefixed names
+#' @param x named vector to make list of
+httr_content_as_list <- function(x, prefix=""){
+  x <- as.list(x)
+  names(x) <- paste0(prefix, names(x))
+  for( i in seq_along(x) ){
+    if( length(x[[i]])==0 ){
+      x[[i]] <- ""
+    }
+  }
+  return(x)
+}
 
